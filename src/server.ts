5ca201165df5bfb7a2a271edb9b2ea1727fafb4c
Gabriel Engel <gfbengel@gmail.com>
2020-04-16 17:27:02 -0300

    projeto final

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 
-const app = express()
+import routes from './routes'
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello world' })
-})
+const app = express()
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('server started on port 3333')
